6a4e2f2bf9a827d9507fe3fd4b4a65d0119e19ae
SusiBaril <rosaleskeegan@gmail.com>
2025-12-03 11:32:25 +0800

    Inventory-Done

--- js/script.js
+++ js/script.js
@@ -37,3 +37,55 @@ new Chart(ctx, {
         }
     }
 });
+
+
+const ctx1 = document.getElementById('myChart1');
+
+new Chart(ctx1, {
+    type: 'pie',
+    data: {
+        labels: [
+            'Red',
+            'Blue',
+            'Yellow'
+        ],
+        datasets: [{
+            label: 'My First Dataset',
+            data: [300, 50, 100],
+            backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 205, 86)'
+            ],
+            hoverOffset: 4
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+const priceInput = document.getElementById('priceInput');
+
+priceInput.addEventListener('blur', function(e) {
+    // Remove all non-digit characters except dot
+    let value = this.value.replace(/[^0-9.]/g, '');
+
+    // Parse as float and format
+    if (value) {
+        value = parseFloat(value).toLocaleString('en-PH', {
+            style: 'currency',
+            currency: 'PHP',
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2
+        });
+    }
+
+    this.value = value;
+});
+
+
