3f35be9ebf62075df1f38fcba246a5a2a0e125e4
SusiBaril <rosaleskeegan@gmail.com>
2025-12-03 09:19:57 +0800

    Dashboard-Done

--- js/script.js
+++ js/script.js
@@ -5,11 +5,35 @@ link.forEach(link => {
 
     let currentPath = window.location.pathname;
 
-    if(currentPath === "/"){
+    if (currentPath === "/") {
         currentPath = "/index.html";
     }
 
-    if(linkHref === currentPath) {
+    if (linkHref === currentPath) {
         link.classList.add('active');
     }
 })
+
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: [
+            'January', 'February', 'March', 'April', 'May', 'June',
+            'July', 'August', 'September', 'October', 'November', 'December'
+        ],
+        datasets: [{
+            label: '# of Incomes',
+            data: [12000, 19000, 3000, 5000, 2000, 3000],
+            borderWidth: 2,
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
